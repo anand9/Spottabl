54649ddf32512748b93cfc5c5ea7d6fccad1fb64
= <=>
2020-02-14 23:15:09 +0530

    add logo

--- src/Components/Layout/TopNav.js
+++ src/Components/Layout/TopNav.js
@@ -17,7 +17,7 @@ const TopNav = (props) => {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Spottabl</NavbarBrand>
+        <NavbarBrand href="/"><img src="https://www.spottabl.com/images/spottabl_logo.png"  height="25"/></NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
